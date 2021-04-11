b8ecf2f324e77de483b58ae82d9ca1ed036cde0d
Peter Farr <Peter@PrismaPhonic.com>
2021-04-10 19:10:34 -0700

    Fix up really awful solution I wrote in the past for first non repeating character.
    Signed-off-by: Peter Farr <Peter@PrismaPhonic.com>

--- interview/arrays/02-firstNotRepeatingCharacter/rs-solution/std-lib-solution/src/lib.rs
+++ interview/arrays/02-firstNotRepeatingCharacter/rs-solution/std-lib-solution/src/lib.rs
@@ -1,31 +1,20 @@
-use std::collections::HashSet;
 use std::collections::HashMap;
 
-fn firstNotRepeatingCharacter(s: String) -> char {
-  let mut occurances: HashSet<char> = HashSet::with_capacity(24);
-  let mut uniq: HashMap<char, usize> = HashMap::with_capacity(24);
- 
-  s.chars().enumerate().for_each(|(i, c)| {
-    if occurances.insert(c) == true {
-      uniq.insert(c, i);
-    } else {
-      uniq.remove(&c);
+fn first_non_repeating_character(s: String) -> char {
+    let mut occurances = HashMap::new();
+
+    for c in s.chars() {
+        let count = occurances.entry(c).or_insert(0);
+        *count += 1;
     }
-  });
-  
-  if uniq.is_empty() {
-    '_'
-  } else {
-    let mut min_idx = s.len();
-    let mut answer = '_';
-    for (k, v) in uniq.into_iter() {
-        if v < min_idx {
-            min_idx = v;
-            answer = k;
+
+    for c in s.chars() {
+        if *occurances.get(&c).unwrap() == 1 {
+            return c;
         }
-    };
-    answer
-  }
+    }
+
+    '_'
 }
 
 #[cfg(test)]
@@ -34,16 +23,16 @@ mod tests {
 
     #[test]
     fn test_1() {
-        assert_eq!(firstNotRepeatingCharacter("abacabad".to_string()), 'c');
+        assert_eq!(first_non_repeating_character("abacabad".to_string()), 'c');
     }
 
     #[test]
     fn test_3() {
-        assert_eq!(firstNotRepeatingCharacter("z".to_string()), 'z');
+        assert_eq!(first_non_repeating_character("z".to_string()), 'z');
     }
 
     #[test]
     fn test_10() {
-        assert_eq!(firstNotRepeatingCharacter("ngrhhqbhnsipkcoqjyviikvxbxyphsnjpdxkhtadltsuxbfbrkof".to_string()), 'g');
+        assert_eq!(first_non_repeating_character("ngrhhqbhnsipkcoqjyviikvxbxyphsnjpdxkhtadltsuxbfbrkof".to_string()), 'g');
     }
 }
